03a27d003c77a932fc248d24fd5ca102b2f7b318
Victor Caldentey <victor.caldentey@scmspain.com>
2017-10-23 13:08:19 +0200

    Refactor using fluent interface

--- src/openads/infrastructure/repository/appnexus/AppNexusAdRepository.js
+++ src/openads/infrastructure/repository/appnexus/AppNexusAdRepository.js
@@ -19,31 +19,30 @@ export default class AppNexusAdRepository extends AdRepository {
    */
   findAd ({adRequest}) {
     return new Promise((resolve, reject) => {
-      this._connector.activateDebugMode()
-
-      this._connector.setPageOpts({
-        member: this._connector.member,
-        keywords: adRequest.segmentation
-      })
-      this._connector.onEvent({
-        event: 'adAvailable',
-        targetId: adRequest.containerId,
-        callback: (adRetrieved) => resolve(this._appNexusResultMapper.mapResponseToDomain({appNexusResponse: adRetrieved}))
-      })
-      this._connector.onEvent({
-        event: 'adBadRequest',
-        targetId: adRequest.containerId,
-        callback: (data) => {
-          reject(data)
-        }
-      })
-      this._connector.defineTag({
-        invCode: adRequest.placement,
-        sizes: adRequest.sizes,
-        targetId: adRequest.containerId
-      })
-
-      this._connector.loadTags()
+      this._connector
+        .activateDebugMode()
+        .setPageOpts({
+          member: this._connector.member,
+          keywords: adRequest.segmentation
+        })
+        .onEvent({
+          event: 'adAvailable',
+          targetId: adRequest.containerId,
+          callback: (adRetrieved) => resolve(this._appNexusResultMapper.mapResponseToDomain({appNexusResponse: adRetrieved}))
+        })
+        .onEvent({
+          event: 'adBadRequest',
+          targetId: adRequest.containerId,
+          callback: (data) => {
+            reject(data)
+          }
+        })
+        .defineTag({
+          invCode: adRequest.placement,
+          sizes: adRequest.sizes,
+          targetId: adRequest.containerId
+        })
+        .loadTags()
     })
   }
 }
