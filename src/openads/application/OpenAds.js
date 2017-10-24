e5d1cea00fd11dd9d9917d0b841e29337fb246b6
Victor Caldentey <victor.caldentey@scmspain.com>
2017-10-24 12:45:42 +0200

    Add direct access to Use case methods from application

--- src/openads/application/OpenAds.js
+++ src/openads/application/OpenAds.js
@@ -3,12 +3,29 @@ export default class OpenAds {
     this._container = container
   }
 
-  findAdsUseCase () {
-    return this._container.getInstance({key: 'FindAdsUseCase'})
+  /**
+   *
+   * @param {AdRequest} adRequest
+   * @param {string} adRequest.position - Position where Ad will be displayed
+   * @param {string} adRequest.containerId - DOM element where Ad will be rendered
+   * @param {string} adRequest.segmentation - Segmentation to target the Ad
+   * @param {string} adRequest.placement - Placement to target the Ad
+   * @param {Array<Size>} adRequest.sizes - Collection of sizes accepted for given targeting segmentation
+   */
+  find ({adRequest}) {
+    return this._container.getInstance({key: 'FindAdsUseCase'}).find({adRequest})
   }
-
-  displayAdsUseCase () {
-    return this._container.getInstance({key: 'DisplayAdsUseCase'})
+  /**
+   *
+   * @param {AdRequest} adRequest
+   * @param {string} adRequest.position - Position where Ad will be displayed
+   * @param {string} adRequest.containerId - DOM element where Ad will be rendered
+   * @param {string} adRequest.segmentation - Segmentation to target the Ad
+   * @param {string} adRequest.placement - Placement to target the Ad
+   * @param {Array<Size>} adRequest.sizes - Collection of sizes accepted for given targeting segmentation
+   */
+  display ({adRequest}) {
+    return this._container.getInstance({key: 'DisplayAdsUseCase'}).display({adRequest})
   }
 
   environment () {
