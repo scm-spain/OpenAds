8658b4bf8bd3f25ea5942b8168b738a10fba7628
Alex Castells Gasia <alex.castells@scmspain.com>
2017-11-08 18:07:48 +0100

    Add test to the remove observer functionality in event dispatcher

--- src/openads/infrastructure/service/EventDispatcher.js
+++ src/openads/infrastructure/service/EventDispatcher.js
@@ -10,22 +10,37 @@ export default class EventDispatcher {
    * @returns {Number}
    */
   addObserver ({eventName, position, observer}) {
+    if (!eventName) {
+      throw new Error('Event Name is required')
+    }
+    if (!position) {
+      throw new Error('Position is required')
+    }
+    if (typeof observer !== 'function') {
+      throw new Error('Observer must be a function')
+    }
     return this._getEventPositionObservers({eventName, position}).push(observer)
   }
 
   removeObserver ({eventName, position, observer}) {
-    this._getEventPositionObservers({eventName, position}).filter(registeredObserver => registeredObserver !== observer)
+    let eventMap = this._getEventMap({eventName})
+    let positionObservers = this._getEventPositionObservers({eventName, position})
+    eventMap.set(position, positionObservers.filter(registeredObserver => registeredObserver !== observer))
   }
 
   dispatch ({eventName, position, payload}) {
     this._getEventPositionObservers({eventName, position}).forEach(observer => observer({payload}))
   }
 
-  _getEventPositionObservers ({eventName, position}) {
+  _getEventMap ({eventName}) {
     if (!this._observers.has(eventName)) {
       this._observers.set(eventName, new Map())
     }
-    const eventMap = this._observers.get(eventName)
+    return this._observers.get(eventName)
+  }
+
+  _getEventPositionObservers ({eventName, position}) {
+    const eventMap = this._getEventMap({eventName})
     if (!eventMap.has(position)) {
       eventMap.set(position, [])
     }
