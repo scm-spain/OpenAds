1f3822e9cbf7694fae4299b9247b759e9f7c29d1
Alex Castells Gasia <alex.castells@scmspain.com>
2017-11-29 15:34:02 +0100

    TEMPORAL hardcoded json response for native integration purposes

--- src/openads/infrastructure/repository/appnexus/AppNexusAdRepository.js
+++ src/openads/infrastructure/repository/appnexus/AppNexusAdRepository.js
@@ -19,30 +19,64 @@ export default class AppNexusAdRepository extends AdRepository {
    */
   findAd ({adRequest}) {
     return new Promise((resolve, reject) => {
-      this._connector
-        .onEvent({
-          event: 'adAvailable',
-          targetId: adRequest.containerId,
-          callback: (adRetrieved) => resolve(this._appNexusResultMapper.mapResponseToDomain({
-            position: adRequest.position,
-            appNexusResponse: adRetrieved
-          }))
-        })
-        .onEvent({
-          event: 'adBadRequest',
-          targetId: adRequest.containerId,
-          callback: (data) => {
-            reject(data)
+      if (adRequest.position.includes('NATIVE')) {
+        resolve(this._appNexusResultMapper.mapResponseToDomain({
+          position: adRequest.position,
+          appNexusResponse: {
+            'adType': 'native',
+            'source': 'rtb',
+            'native': {
+              'title': 'Hardcoding Creativities for OpenAds',
+              'body': 'The better option is to get it from the AdServer but we need now to develop the render capability until requesting is complete',
+              'icon': {
+                'width': 50,
+                'height': 50,
+                'url': 'https://s.dcdn.es.s3.amazonaws.com/mightyducks/test-images/50x50.png'
+              },
+              'image': {
+                'width': 300,
+                'height': 300,
+                'url': 'https://s.dcdn.es.s3.amazonaws.com/mightyducks/test-images/300x300.png'
+              },
+              'cta': 'Give me a Click',
+              'sponsoredBy': 'The Mighty Ducks',
+              'impressionTrackers': [
+                'https://s.dcdn.es.s3.amazonaws.com/mightyducks/test-images/1x1.000.png'
+              ],
+              'clickTrackers': [
+                'https://s.dcdn.es.s3.amazonaws.com/mightyducks/test-images/1x1-f00.png'
+              ],
+              'clickUrl': 'https://github.com/scm-spain/OpenAds',
+              'clickFallbackUrl': ''
+            }
           }
-        })
-        .defineTag({
-          member: this._connector.member,
-          targetId: adRequest.containerId,
-          invCode: adRequest.placement,
-          sizes: adRequest.sizes,
-          keywords: adRequest.segmentation
-        })
-        .loadTags()
+        }))
+      } else {
+        this._connector
+              .onEvent({
+                event: 'adAvailable',
+                targetId: adRequest.containerId,
+                callback: (adRetrieved) => resolve(this._appNexusResultMapper.mapResponseToDomain({
+                  position: adRequest.position,
+                  appNexusResponse: adRetrieved
+                }))
+              })
+              .onEvent({
+                event: 'adBadRequest',
+                targetId: adRequest.containerId,
+                callback: (data) => {
+                  reject(data)
+                }
+              })
+              .defineTag({
+                member: this._connector.member,
+                targetId: adRequest.containerId,
+                invCode: adRequest.placement,
+                sizes: adRequest.sizes,
+                keywords: adRequest.segmentation
+              })
+              .loadTags()
+      }
     })
   }
 
