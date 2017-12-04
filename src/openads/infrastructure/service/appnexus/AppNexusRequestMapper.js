fb207405d4790f813aad979809844eee4c8bb0e2
rafael-marquez <rafael.marquez@scmspain.com>
2017-12-04 18:29:14 +0100

    Native in AppNexus Requests:
    - New AppNexus request mapper to build request including Native ads. Supporting the following request types: text, image and url.
    - Remove current native mock responses.

---
+++ src/openads/infrastructure/service/appnexus/AppNexusRequestMapper.js
@@ -0,0 +1,65 @@
+export default class AppNexusRequestMapper {
+  mapDomainToRequest ({member, targetId, invCode, sizes, keywords, native}) {
+    let request = {
+      member: member,
+      targetId: targetId,
+      invCode: invCode,
+      sizes: sizes,
+      keywords: keywords
+    }
+
+    if (native) {
+      request.native = {}
+      Object.entries(native).forEach(([key, value]) => {
+        if (value) {
+          switch (value.type) {
+            case 'text': {
+              request.native[key] = this._mapNativeText({value})
+              break
+            }
+            case 'image': {
+              request.native[key] = this._mapNativeImage({value})
+              break
+            }
+            case 'url': {
+              request.native[key] = this._mapNativeUrl({value})
+              break
+            }
+            default:
+              throw new Error(`Unsupported native value type ${value.type}`)
+          }
+        } else {
+          request.native[key] = {}
+        }
+      })
+    }
+    return request
+  }
+
+  _mapNativeText ({value}) {
+    let result = {}
+    if (value.required) {
+      result.required = true
+    }
+    if (value.max) {
+      result.max_length = value.max
+    }
+    return result
+  }
+
+  _mapNativeImage ({value}) {
+    let result = {}
+    if (value.required) {
+      result.required = true
+    }
+    return result
+  }
+
+  _mapNativeUrl ({value}) {
+    let result = {}
+    if (value.required) {
+      result.required = true
+    }
+    return result
+  }
+}
