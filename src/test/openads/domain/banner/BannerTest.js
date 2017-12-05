c5c84cebf4f2d92362a3f731f58f4ca2003af225
Alex Castells Gasia <alex.castells@scmspain.com>
2017-12-05 15:36:41 +0100

    Add more test coverage

--- src/test/openads/domain/banner/BannerTest.js
+++ src/test/openads/domain/banner/BannerTest.js
@@ -2,9 +2,32 @@
 import {expect} from 'chai'
 import Banner from '../../../../openads/domain/ad/banner/Banner'
 
-describe('Banner', function () {
-  describe('Given a banner instance', function () {
-    it('Calling show method should dispatch the PRE_RENDER event before calling the renderer', function () {
+describe('Banner', () => {
+  describe('Given valid data to constructor', () => {
+    it('Should create a Banner which getters give the given data', () => {
+      const givenContainerId = '1'
+      const givenPosition = '2'
+      const givenSource = '3'
+      const givenContent = '4'
+      const givenSize = [1, 1]
+
+      const banner = new Banner({
+        containerId: givenContainerId,
+        position: givenPosition,
+        source: givenSource,
+        content: givenContent,
+        size: givenSize
+      })
+
+      expect(banner.containerId).to.equal(givenContainerId)
+      expect(banner.position).to.equal(givenPosition)
+      expect(banner.source).to.equal(givenSource)
+      expect(banner.content).to.equal(givenContent)
+      expect(banner.size).to.deep.equal(givenSize)
+    })
+  })
+  describe('Given a banner instance', () => {
+    it('Calling show method should dispatch the PRE_RENDER event before calling the renderer', () => {
       const givenContainerId = 'pos_1_id'
       const givenPosition = 'POS1'
 
--- src/test/openads/domain/native/NativeTest.js
+++ src/test/openads/domain/native/NativeTest.js
@@ -3,6 +3,26 @@ import {expect} from 'chai'
 import Native from '../../../../openads/domain/ad/native/Native'
 
 describe('Native', () => {
+  describe('Given valid data to constructor', () => {
+    it('Should create a Native which getters give the given data', () => {
+      const givenContainerId = '1'
+      const givenPosition = '2'
+      const givenSource = '3'
+      const givenJson = {a: 'b'}
+
+      const native = new Native({
+        containerId: givenContainerId,
+        position: givenPosition,
+        source: givenSource,
+        json: givenJson
+      })
+
+      expect(native.containerId).to.equal(givenContainerId)
+      expect(native.position).to.equal(givenPosition)
+      expect(native.source).to.equal(givenSource)
+      expect(native.json).to.deep.equal(givenJson)
+    })
+  })
   describe('Given a native instance', () => {
     it('Calling show method should dispatch the PRE_RENDER event before calling the renderer', () => {
       const givenContainerId = 'pos_1_id'
