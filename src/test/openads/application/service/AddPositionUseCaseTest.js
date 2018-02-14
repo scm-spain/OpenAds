d1b7c7c5e7f84b64898ef210e53cc18370f6d8c0
Victor Caldentey <victor.caldentey@scmspain.com>
2018-02-14 16:21:41 +0100

    Add AddPositionUseCase test

---
+++ src/test/openads/application/service/AddPositionUseCaseTest.js
@@ -0,0 +1,47 @@
+
+import {expect} from 'chai'
+import sinon from 'sinon'
+import DisplayAdsUseCase from '../../../../openads/application/service/DisplayAdsUseCase'
+import AddPositionUseCase from '../../../../openads/application/service/AddPositionUseCase'
+import PositionFactory from '../../../../openads/domain/position/PositionFactory'
+import ProxyPositionFactory from '../../../../openads/infrastructure/position/ProxyPositionFactory'
+
+describe('Add Position use case', function () {
+  describe('given a valid Position DTO', function () {
+    it('should return a Promise', function () {
+      const givenPositionDTO = {}
+      const positionRepositoryMock = {}
+      const positionFactory = new ProxyPositionFactory({proxyHandler: {}})
+
+      const addPositionUseCase = new AddPositionUseCase({
+        positionRepository: positionRepositoryMock,
+        positionFactory: positionFactory
+      })
+      expect(addPositionUseCase.addPosition(givenPositionDTO)).to.be.a('promise')
+    })
+
+    it('should call to position factory and position repository once', function (done) {
+      const givenPositionRequest = {}
+      const positionRepositoryMock = {
+        save: ({position}) => Promise.resolve({
+          show: () => null
+        })
+      }
+      const positionRepositorySpy = sinon.spy(positionRepositoryMock, 'save')
+
+      const positionFactory = new ProxyPositionFactory({proxyHandler: {}})
+      const positionFactorySpy = sinon.spy(positionFactory, 'create')
+      const addPositionUseCase = new AddPositionUseCase({
+        positionRepository: positionRepositoryMock,
+        positionFactory: positionFactory
+      })
+      addPositionUseCase.addPosition(givenPositionRequest)
+        .then(() => {
+          expect(positionRepositorySpy.calledOnce, 'repository should be called once').to.be.true
+          expect(positionFactorySpy.calledOnce, 'factory should be called once').to.be.true
+          done()
+        })
+        .catch(error => done(error))
+    })
+  })
+})
