1b14fdbf2c1a32a728ddf571bf2b0980282e55a7
Victor Caldentey <victor.caldentey@scmspain.com>
2018-02-19 11:55:10 +0100

    InMemoryPosition tests * Add tests for in memory position repository * Refactor repository using && operator * Refactor Position already exists error to include stack trace and name

--- src/openads/domain/position/PositionAlreadyExists.js
+++ src/openads/domain/position/PositionAlreadyExists.js
@@ -1,5 +1,8 @@
 export default class PositionAlreadyExists extends Error {
   constructor ({id}) {
-    super(`Position ${id} already exists`)
+    super()
+    this.name = 'PositionAlreadyExists'
+    this.message = `Position ${id} already exists`
+    this.stack = (new Error()).stack
   }
 }
---
+++ src/test/openads/infrastructure/position/InMemoryPositionRepositoryTest.js
@@ -0,0 +1,160 @@
+import {expect} from 'chai'
+import sinon from 'sinon'
+import InMemoryPositionRepository from '../../../../openads/infrastructure/position/InMemoryPositionRepository'
+import ProxyPositionFactory from '../../../../openads/infrastructure/position/ProxyPositionFactory'
+import {POSITION_NOT_VISIBLE} from '../../../../openads/domain/position/positionStatus'
+
+describe('InMemory Position Repository', function () {
+  describe('Given a Domain position', function () {
+    it('should return a Promise', function () {
+      const positionFactory = new ProxyPositionFactory({proxyHandler: {}})
+      const givenPosition = positionFactory.create({
+        id: '42',
+        name: 'lala',
+        source: 'appnexus',
+        placement: 'blabla',
+        segmentation: 'adsasd',
+        sizes: [],
+        native: {},
+        status: POSITION_NOT_VISIBLE
+      })
+      const inMemoryPositionRepository = new InMemoryPositionRepository()
+      expect(inMemoryPositionRepository.save({position: givenPosition})).to.be.a('promise')
+    })
+
+    it('should save it and return a Promise', function () {
+      const positionFactory = new ProxyPositionFactory({proxyHandler: {}})
+      const givenPosition = positionFactory.create({
+        id: '42',
+        name: 'lala',
+        source: 'appnexus',
+        placement: 'blabla',
+        segmentation: 'adsasd',
+        sizes: [],
+        native: {},
+        status: POSITION_NOT_VISIBLE
+      })
+      const inMemoryPositionRepository = new InMemoryPositionRepository()
+      inMemoryPositionRepository.save({position: givenPosition})
+        .then(position => expect(position.id, 'position not saved').to.equal(givenPosition.id))
+    })
+  })
+  describe('Given two positions already saved in memory', function () {
+    it('should find them and return Promise', function (done) {
+      const inMemoryPositionRepository = new InMemoryPositionRepository({
+        positions: [
+          ['42', {
+            id: '42',
+            name: 'lala',
+            source: 'appnexus',
+            placement: 'blabla',
+            segmentation: 'adsasd',
+            sizes: [],
+            native: {},
+            status: POSITION_NOT_VISIBLE
+          }],
+          ['43', {
+            id: '43',
+            name: 'lala43',
+            source: 'google',
+            placement: 'jarjar',
+            segmentation: 'lala=true',
+            sizes: [],
+            native: {},
+            status: POSITION_NOT_VISIBLE
+          }]
+        ]
+      })
+      Promise.all([
+        inMemoryPositionRepository.find({id: '42'}),
+        inMemoryPositionRepository.find({id: '43'})
+      ])
+        .then(([exists1, exists2]) => {
+          expect(exists1.id, 'position not saved').to.equal('42')
+          expect(exists2.id, 'position not saved').to.equal('43')
+          done()
+        })
+        .catch(error => done(error))
+    })
+
+    it('should not find position and return Promise with null value', function (done) {
+      const inMemoryPositionRepository = new InMemoryPositionRepository({
+        positions: [
+          ['42', {
+            id: '42',
+            name: 'lala',
+            source: 'appnexus',
+            placement: 'blabla',
+            segmentation: 'adsasd',
+            sizes: [],
+            native: {},
+            status: POSITION_NOT_VISIBLE
+          }],
+          ['43', {
+            id: '43',
+            name: 'lala43',
+            source: 'google',
+            placement: 'jarjar',
+            segmentation: 'lala=true',
+            sizes: [],
+            native: {},
+            status: POSITION_NOT_VISIBLE
+          }]
+        ]
+      })
+
+      inMemoryPositionRepository.find({id: '4242'})
+        .then((exists) => {
+          expect(exists, 'position not saved').to.be.false
+          done()
+        })
+        .catch(error => done(error))
+    })
+
+    it('should return a PositionAlreadyExists error', function (done) {
+      const inMemoryPositionRepository = new InMemoryPositionRepository({
+        positions: [
+          ['42', {
+            id: '42',
+            name: 'lala',
+            source: 'appnexus',
+            placement: 'blabla',
+            segmentation: 'adsasd',
+            sizes: [],
+            native: {},
+            status: POSITION_NOT_VISIBLE
+          }],
+          ['43', {
+            id: '43',
+            name: 'lala43',
+            source: 'google',
+            placement: 'jarjar',
+            segmentation: 'lala=true',
+            sizes: [],
+            native: {},
+            status: POSITION_NOT_VISIBLE
+          }]
+        ]
+      })
+
+      const positionFactory = new ProxyPositionFactory({proxyHandler: {}})
+      const givenPosition = positionFactory.create({
+        id: '42',
+        name: 'lala',
+        source: 'appnexus',
+        placement: 'blabla',
+        segmentation: 'adsasd',
+        sizes: [],
+        native: {},
+        status: POSITION_NOT_VISIBLE
+      })
+
+      inMemoryPositionRepository.save({position: givenPosition})
+        .then(position => done(new Error('should throw an error')))
+        .catch(error => {
+          expect(error.name, 'Error type is not PositionAlreadyExists').to.equal('PositionAlreadyExists')
+          done()
+        })
+    })
+  })
+})
