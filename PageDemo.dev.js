b2b09f50fb0a10d2f47beb451108f57f1d35673f
Victor Caldentey <victor.caldentey@scmspain.com>
2018-02-26 15:44:33 +0100

    Updates

--- PageDemo.dev.js
+++ PageDemo.dev.js
@@ -60,12 +60,12 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 180);
+/******/ 	return __webpack_require__(__webpack_require__.s = 187);
 /******/ })
 /************************************************************************/
 /******/ ({
 
-/***/ 180:
+/***/ 187:
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
