4b40887a090fb5b0a4ccaed7638e59cee5011001
Victor Caldentey <victor.caldentey@scmspain.com>
2018-03-02 12:56:27 +0100

    Remove positionVisible filter

--- src/openads/application/service/RefreshPositionUseCase.js
+++ src/openads/application/service/RefreshPositionUseCase.js
@@ -28,8 +28,7 @@ export default class RefreshPositionUseCase {
     return this._positionRepository.find({id})
       .then(optionalPosition => ({id, position: optionalPosition}))
       .then(this._filterPositionExists)
-      .then(this._filterPositionVisible)
-      .then(visiblePosition => this._setAdFetchWorkInProgress(visiblePosition))
+      .then(position => this._setAdFetchWorkInProgress(position))
       .then(positionToBeUpdated => positionToBeUpdated.changeSegmentation({...position}))
       .then(changedPosition => this._setAdToPosition(changedPosition))
       .then(positionWithAd => this._positionRepository.saveOrUpdate({position: positionWithAd}))
@@ -42,14 +41,6 @@ export default class RefreshPositionUseCase {
     }
     return optionalPositionWithId.position
   }
-
-  _filterPositionVisible (position) {
-    if (POSITION_NOT_VISIBLE === position.status) {
-      throw new PositionNotVisibleException({position})
-    }
-    return position
-  }
-
   _setAdFetchWorkInProgress (position) {
     this._adRepository.remove({id: position.id})
     return position
