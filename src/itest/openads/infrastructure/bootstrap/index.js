ff36bc505d2373de731867d38aaeb84564cb7fa1
rafael-marquez <rafael.marquez@scmspain.com>
2018-05-09 15:31:04 +0200

    * fix remove appNexusConnector from constructor as is not used anymore.

--- src/itest/openads/infrastructure/bootstrap/index.js
+++ src/itest/openads/infrastructure/bootstrap/index.js
@@ -2,7 +2,7 @@ import ContainerTest from '../configuration/ContainerTest'
 import OpenAds from '../../../../openads/application/OpenAds'
 
 export default class BootstrapTest {
-  static init ({config, appNexusConnector, appNexusClient}) {
-    return new OpenAds({container: new ContainerTest({config, appNexusConnector, appNexusClient})})
+  static init ({config, appNexusClient}) {
+    return new OpenAds({container: new ContainerTest({config, appNexusClient})})
   }
 }
