e923cd78f990de4ef717d6e28a2bb771ef0c2329
Pablo Vega Uceta <pablovegau@gmail.com>
2018-05-09 10:12:16 +0200

    Added itest appNexusClientMock methods counters

--- src/itest/openads/infrastructure/connector/AppNexusClientMock.js
+++ src/itest/openads/infrastructure/connector/AppNexusClientMock.js
@@ -7,11 +7,16 @@ export default class AppNexusClientMock extends AppNexusClient {
     this._refresh = refresh || loadTags
     this._callbackLoadTags = null
     this._callbackRefresh = null
-    this._debounce = undefined
-    this._buffer = undefined
+    // this._debounce = undefined
+    // this._buffer = undefined
+    this.numberOfCallsDefineTag = 0
     this.numberOfCallsToLoadTags = 0
+    this.numberOfCallsToShowTag = 0
     this.numberOfCallsToRefresh = 0
-    this._bufferAccumulator = []
+    this.numberOfCallsToOffEvent = 0
+    this.numberOfCallsToSetPageOpts = 0
+    this.numberOfCallsToClearReques = 0
+    // this._bufferAccumulator = []
     this.debug = false
     this.anq = {
       push: () => { console.log('anq.push') }
@@ -20,36 +25,43 @@ export default class AppNexusClientMock extends AppNexusClient {
 
   defineTag ({member, targetId, invCode, sizes, keywords, native}) {
     console.log('defineTags')
+    this.numberOfCallsDefineTag++
     return this
   }
 
   loadTags () {
     console.log('loadTags')
+    this.numberOfCallsToLoadTags++
     return this
   }
 
   showTag ({target}) {
     console.log('showtag')
+    this.numberOfCallsToShowTag++
     return this
   }
 
   refresh (target) {
     console.log('refresh')
+    this.numberOfCallsToRefresh++
     return this
   }
 
   offEvent (event, targetId) {
     console.log('offEvent')
+    this.numberOfCallsToOffEvent++
     return this
   }
 
   setPageOpts ({member, keywords}) {
     console.log('setPageOpts')
+    this.numberOfCallsToSetPageOpts++
     return this
   }
 
   clearRequest () {
     console.log('clearRequest')
+    this.numberOfCallsToClearReques++
     return this
   }
 }
