aedb4bd3fcfd35282b8fbb33565081b482d5b94a
Victor Caldentey <victor.caldentey@scmspain.com>
2018-10-04 10:16:50 +0200

    Fix integration tests

--- src/itest/openads/infrastructure/configuration/ContainerTest.js
+++ src/itest/openads/infrastructure/configuration/ContainerTest.js
@@ -4,11 +4,17 @@ import {JSDOM} from 'jsdom'
 
 export default class ContainerTest extends Container {
   constructor ({config, eager = true} = {}) {
-    super({config, eager: false})
+    super({
+      config,
+      eager: false,
+      currentWindow: new HTMLDOMDriver({
+        dom: new JSDOM('<!DOCTYPE html><div id="forlayo">Hello world</div>').window
+      })
+    })
     if (eager) super._buildEagerSingletonInstances()
   }
 
   _buildDOMDriver () {
-    return new HTMLDOMDriver({dom: new JSDOM('<!DOCTYPE html><div id="forlayo">Hello world</div>').window.document})
+    return this._currentWindow
   }
 }
