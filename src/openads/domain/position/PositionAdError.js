2f76f30310aebeddd798e5e57e061fd828bac4aa
Alex Castells <alextremp@hotmail.com>
2020-08-13 14:54:58 +0200

    keep errMessage as primary error

--- src/openads/domain/position/PositionAdError.js
+++ src/openads/domain/position/PositionAdError.js
@@ -3,11 +3,11 @@ export default class PositionAdError extends Error {
     super()
     this.name = 'PositionAdError'
     const errorMessage =
-      position.ad &&
-      position.ad.data &&
-      (typeof position.ad.data === 'string'
-        ? position.ad.data
-        : position.ad.data.errMessage || 'unexpected error')
+      (position.ad &&
+        position.ad.data &&
+        (position.ad.data.errMessage ||
+          (typeof position.ad.data === 'string' && position.ad.data))) ||
+      'unexpected error'
     this.message = `Error loading Ad ${position.id}: ${errorMessage}.`
     this.stack = new Error().stack
     this.position = position
